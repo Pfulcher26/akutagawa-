aefe1894eef0a2db9f221da9124f2c7e946a5a3d
Payne Fulcher <pfulcher26@gmail.com>
2020-06-05 10:27:51 -0700

    Attaches event listener to value of variable 2 input

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
       const variable1 = $("input:radio[name=variable1]:checked").val();
+      const variable2 = $("input:radio[name=variable2]:checked").val();
+      
   }
 }
