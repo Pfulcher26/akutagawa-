f1573c796773de3ae44043a5832f5dabb6ebe83c
Payne Fulcher <pfulcher26@gmail.com>
2020-06-05 10:58:04 -0700

    Adds if statement for variable 1 with the command to display the Ruby class if the first value of each variable is selected

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
       const variable4 = $("input:radio[name=variable4]:checked").val();
       const variable5 = $("input:radio[name=variable5]:checked").val();
       const variable6 = $("input:radio[name=variable6]:checked").val();
-      $(".Ruby").toggle();
+      if (variable1 === "the master" & variable2 === "the woman" & variable3 === "father" & variable4 === "tedium" & variable5 === "tokyo" & variable6 === "sparks") {
+        $(".Ruby").show();
+      }
   });
 });
