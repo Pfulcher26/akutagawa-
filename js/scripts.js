d2c3ec146a71ca2514d9ad39993dfc968244c3da
Payne Fulcher <pfulcher26@gmail.com>
2020-06-05 10:29:53 -0700

    Attaches event listener to value of variables 4, 5, and 6  input

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
     event.preventDefault();
       const variable1 = $("input:radio[name=variable1]:checked").val();
       const variable2 = $("input:radio[name=variable2]:checked").val();
-      const variable3 = $("input:radio[name=variable3]:checked").val()
+      const variable3 = $("input:radio[name=variable3]:checked").val();
+      const variable4 = $("input:radio[name=variable4]:checked").val();
+      const variable5 = $("input:radio[name=variable5]:checked").val();
+      const variable6 = $("input:radio[name=variable6]:checked").val();
 
   }
 }
